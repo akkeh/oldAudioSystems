140104a12bc11324c26d1307da0e0c37dfe1376b
akkeh <akkeaccu@msn.com>
2014-12-15 14:54:53 +0100

    initial commit

---
+++ azath.h
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdlib>
+
+#ifndef AZATH_H_
+#define AZATH_H_
+
+#define FS 44100
+#define TWOPI M_PI+M_PI
+struct sig {
+    sig* data;
+    unsigned long N;
+};
+struct sig1d : public sig {
+    double* re;
+    double* im;
+    unsigned long N;
+};
+
+sig read(const char* filename, int print = 0);
+int write(const char* filename, sig const& in_sig, int print = 0);
+
+#endif
